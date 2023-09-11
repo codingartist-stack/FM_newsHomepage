1750a96fca5028fcab83a1cd89b584ef067432ff
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-11 15:43:38 -0700

    working on the menu to toggle

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const navToggle = document.querySelector('.mobileToggle');
-const nav = document.querySelector('primaryNav');
+const primaryNav = document.querySelector('.primaryNav');
 const navBackground = document.querySelector('.navBackgroup');
+
+navToggle.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', true);
+    navBackground.setAttribute('data-visible', true);
+  } else {
+    primaryNav.setAttribute('data-visible', false);
+    navBackground.setAttribute('data-visible', false);
+  }
+});
